ef0fdb12d7c31d5bc506e2375f3a1fc85daf9bfe
jcollard <josephmcollard@gmail.com>
2014-11-08 15:51:50 -0500

    Added basis for collision stuff.

--- State.elm
+++ State.elm
@@ -2,5 +2,10 @@ module State where
 
 type State = Int
 
+type Position = { x : Float, y : Float }
+type BoundingBox = { x : Float, y : Float, width : Float, height : Float }
+
+type Object a = { pos : Position, box : BoundingBox }
+
 initialState : State
 initialState = 0
