3ea9218f7874a94ce8a300bc355b89ae239e6e2f
jcollard <josephmcollard@gmail.com>
2014-11-08 15:16:17 -0500

    Update does stuff.

--- Update.elm
+++ Update.elm
@@ -1,9 +1,15 @@
 module Update where
 import Playground (..)
 import Playground.Input (..)
+import Keyboard.Keys as Keys
 import State
 
 type State = State.State
 
 update : RealWorld -> Input -> State -> State
-update rw input state = state
+update rw input state = 
+    case input of
+      Key k -> if | k `Keys.equals` Keys.arrowUp -> state + 1
+                  | k `Keys.equals` Keys.arrowDown -> state - 1
+                  | otherwise -> state
+      otherwise -> state
