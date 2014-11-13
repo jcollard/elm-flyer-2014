cb50fa20321fee4a7eb6312e53de89b0554dca99
jcollard <josephmcollard@gmail.com>
2014-11-13 09:39:34 -0500

    Added simple straight movement.

--- Enemy/Generator.elm
+++ Enemy/Generator.elm
@@ -2,7 +2,7 @@ module Enemy.Generator where
 
 import Util (..)
 import Enemy.AlienWarrior as AlienWarrior
-import Enemy (Enemy, enemy)
+import Enemy (Enemy, EnemyTraits, enemy, checkDestroyed)
 import Generator.Standard (..)
 import Generator
 import Generator (int32Range, floatRange, float)
@@ -18,7 +18,7 @@ theGenerator n = { generator = generator n,
 
 nextWave : Generator -> ([Enemy], Generator)
 nextWave g = 
-    let (num_enemies, g') = int32Range (g.wave, g.wave*g.wave) g.generator
+    let (num_enemies, g') = int32Range (g.wave, 5*g.wave) g.generator
         (enemies, g'') = createEnemies g.wave num_enemies g'
     in (enemies, {g | generator <- g'', wave <- g.wave + 1})
 
@@ -38,17 +38,44 @@ createEnemies' es wave num_enemies g =
                          num_enemies' = num_enemies - (length new)
                      in createEnemies' (new ++ es) wave num_enemies' g'
 
+-- Groups
+
 single : Int -> RndGen -> (Enemy, RndGen)
 single wave g = 
     let 
         (x_off, g') = floatRange (100, 1000) g
         (y_off, g'') = floatRange (-100, 100) g'
         pos = { x = screenBounds.right + x_off, y = 0 + y_off }
+        (passive, g''') = movement wave g''
         ctor = AlienWarrior.spawn
         enemy = ctor pos
-    in (enemy, g'')
+        enemy' = { enemy | passive <- passive pos }
+    in (enemy', g''')
 
 train : Int -> Int -> RndGen -> ([Enemy], RndGen)
 train wave n g = ([], g)
                          
-              
+         
+-- Movements
+
+type PassiveFunction = Location -> Time -> EnemyTraits -> EnemyTraits
+
+movement : Int -> RndGen -> (PassiveFunction, RndGen)
+movement wave g =
+    let (p, g') = float g
+    in if | p <= 1 -> 
+              let (speed, g'') = floatRange (1, 10) g'
+              in (straight speed, g'')
+          | otherwise -> (straight 5, g')
+
+straight : Float -> Location -> Time -> EnemyTraits -> EnemyTraits
+straight speed {x, y} dt traits =
+         let t = traits.time
+             t' = t + dt
+             x' = x - speed*t
+             y' = y
+             pos' = { x = x', y = y' }
+         in checkDestroyed { traits |
+                             time <- t'
+                           , pos <- pos'
+                           }
