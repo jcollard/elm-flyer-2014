fe37a1cf49cc75a0c61821144fe5c3f012e8931a
jcollard <josephmcollard@gmail.com>
2014-11-12 19:50:39 -0500

    Updated the way Nuke works

--- Missile/Nuke.elm
+++ Missile/Nuke.elm
@@ -17,17 +17,21 @@ fire pos =
                             damage = 5,
                             cooldown = 150 }
         traits = missile.traits
-    in [ { missile | passive <- passive } ]
+        create_missile t = { missile | passive <- passive pos t }
+    in map create_missile [0..19]
 
-passive : Time -> MissileTraits -> MissileTraits
-passive dt traits = 
+passive : Location -> Time -> Time -> MissileTraits -> MissileTraits
+passive { x , y } modifier dt traits = 
     let t = traits.time 
+        t_mod = t - modifier
         t' = t + dt
-        vel' = { x = 3, y = 10 * (cos << degrees <| 5*t) }
-        dim' = { width = t, height = t }
+        x' = x + t_mod*5
+        y' = y + 100*sin(degrees <| 5*t_mod)
+        pos' = { x = x', y = y' }
+        dim' = { width = max 0 t_mod, height = max 0 t_mod }
         form' = toForm (image (round dim'.width) (round dim'.height) "../assets/standard-missile.gif")
     in { traits | time <- t', 
-                  vel <- vel',
+                  pos <- pos',
                   dim <- dim',
                   form <- form'
                     }
