d2c0ce02afbd7ad45c485ddb4714e43c8fa89530
jcollard <josephmcollard@gmail.com>
2014-11-12 19:38:38 -0500

    Updated the way split shot works.

--- Missile/SplitShot.elm
+++ Missile/SplitShot.elm
@@ -23,15 +23,17 @@ fire pos =
                             time = 0,
                             damage = 5,
                             cooldown = 15 }
-    in [{ missile | passive <- passive 1 },
-        { missile | passive <- passive (-1) }]
+    in [{ missile | passive <- passive pos 1 },
+        { missile | passive <- passive pos (-1) }]
 
-passive : Float -> Time -> MissileTraits -> MissileTraits
-passive modifier dt traits = 
+passive : Location -> Float -> Time -> MissileTraits -> MissileTraits
+passive { x, y } modifier dt traits = 
     let t = traits.time 
         t' = t + dt
         distance = 10
-        vel' = { x = min t 15, y = if t <= distance then 0 else modifier * (min (t - distance) 5) }
+        x' = x + (t/2)^2
+        y' = if t < 15 then y else y + modifier * (t - 15)^(1.4)
+        pos' = { x = x', y = y' }
     in { traits | time <- t', 
-                  vel <- vel'
+                  pos <- pos'                         
        }
