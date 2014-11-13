be458a54803869725c857f2b7d037dca3be3991d
jcollard <josephmcollard@gmail.com>
2014-11-13 15:23:24 -0500

    Worked on powerups

---
+++ PowerUp.elm
@@ -0,0 +1,29 @@
+module PowerUp where
+
+import Object (..)
+import Util (..)
+import Missile (Missile)
+
+type AdditionalTraits = { powerup : Location -> [Missile] }
+
+type PowerUpTraits = Traits AdditionalTraits
+
+type PowerUp = Object AdditionalTraits {}
+
+defaultPowerUp : PowerUpTraits
+defaultPowerUp = { destroyed = False
+                 , dim = { width = 25, height = 25 }
+                 , pos = { x = 0, y = 0 }
+                 , form = circle 0 |> filled red
+                 , powerup = (\ l -> [] ) }
+
+powerup : PowerUpTraits -> PowerUp
+powerup traits = 
+    let p = object traits
+    in { p | passive <- passive }
+
+passive : Time -> PowerUpTraits -> PowerUpTraits
+passive t traits =
+    { traits |
+      pos <- { x = traits.pos.x - 3*t, y = 0 } 
+    }
